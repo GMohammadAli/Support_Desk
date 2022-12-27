0469e3bc4e9616d30ff48417c1725e17ec1c8e23
GMohammadAli <gulammohammadali@gmail.com>
2022-12-27 16:42:42 +0530

    Redux Register and Login functionality

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,9 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { FaUser } from "react-icons/fa"
 import {toast} from "react-toastify"
+import { useSelector, useDispatch } from "react-redux"
+import { register, reset } from "../features/auth/authSlice"
+import { useNavigate } from "react-router-dom"
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -11,6 +14,26 @@ function Register() {
   })
 
   const {name, email, password, password2} = formData
+  
+  const dispatch = useDispatch()
+  
+  const navigate = useNavigate();
+
+  const {user, isLoading, isSuccess, isError, message} = useSelector(state => state.auth)
+  
+  useEffect(() => {
+    if(isError) {
+      toast.error(message)
+    } 
+
+    //Redirect when logged in
+    if(isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+  }, [isError, isSuccess, user, message, navigate, dispatch])
+
   const onChange = (e) => {
     setFormData((prevState) => ({
        ...prevState,
@@ -22,7 +45,15 @@ function Register() {
     e.preventDefault()
 
     if(password !== password2){
-        toast.error("Passwords do not match")
+      toast.error("Passwords do not match")
+    } else {
+      const userData = {
+        name,
+        email,
+        password
+      }
+
+      dispatch(register(userData))
     }
   }
 
