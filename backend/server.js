714eb4e75385c356150a07955c1ff2b334b61451
GMohammadAli <gulammohammadali@gmail.com>
2023-01-04 21:39:38 +0530

    Heroku Build Complete

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require("express")
 const colors = require('colors')
 const dotenv = require("dotenv").config()
@@ -18,13 +19,22 @@ app.use(express.json())
 //payloads and is based on body-parser.
 app.use(express.urlencoded({extended: false}))
 
-app.get('/',(req,res)=> {
-    res.status(200).json({ message: "Welcome to the Support Desk API" });
-})
 
 //Routes
 app.use('/api/users',require("./routes/userRoutes"))
 app.use("/api/tickets", require("./routes/ticketRoutes"));
+
+// Serve Frontend
+if(process.env.NODE_ENV === 'production') {
+    // Set build folder as static
+    app.use(express.static(path.join(__dirname,"../frontend/build")))
+
+    app.get('*', (req,res) => res.sendFile(__dirname,'../','frontend','build','index.html'))
+} else {
+    app.get("/", (req, res) => {
+      res.status(200).json({ message: "Welcome to the Support Desk API" });
+    });
+}
 
 app.use(errorHandler)
 
